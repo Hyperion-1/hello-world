6d55f90637fb862d959c9a3495daa34aac1eb459
Hyperion-1 <93272886+Hyperion-1@users.noreply.github.com>
2021-10-28 20:10:31 -0500

    Update and rename README.md to README.java

---
+++ README.java
@@ -0,0 +1,44 @@
+/*
+# hello-world
+Obligatory tutorial repository
+
+
+A wise man once said that a journey of a thousand miles begins with a single step. While this simple branch/main is trivial, it is good to get the foundations of version control. I learned in school how much being able to 'cntrl-z' a project can matter. 
+
+Also because the tutorial said so: 
+
+Primary interest and language of choice is java, what I was tought in school and what I know. Also through school I have experience with css, html, javascript, mySql, and C++. Further I have some experience with Linux and drivers through my time with the robotics team, something that I would like to get back into later on down the road.
+
+
+2021_10_28 @ 19:43|| added breif description and personal info 
+*/
+
+
+/*
+ * PACKAGE: com.example [I don't understand packages...*yet*, but I want to get used to adding this to the docs]
+ * FILE: helloWorld
+ * AUTHOR: Bradley M. WALKER ▐ 2021_10_18 @ ~01:15
+ * ABSTRACT: Self-evident system print to test IDE functionality and ensure this thing can actually compile,
+ *            and of course, you simply must 'hello world'
+ * LAST EDIT: 2021_10_24 @ 04:49
+ * _>added to github for practice and for posterity 
+ */
+
+package com.example.helloworld;
+//import java.awt.*;
+
+
+
+
+public class helloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("test");
+        System.out.println("Good lord I think it's working?\n");
+        System.out.println("It's working.");
+        System.out.println("And as is tradition: \n Hello world ");
+
+
+    }//end of main
+}//end of file
