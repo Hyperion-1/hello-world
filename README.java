6d4a2c0a077556c47a8f45c8e356e12b1b0626a7
Hyperion-1 <93272886+Hyperion-1@users.noreply.github.com>
2021-10-28 20:11:24 -0500

    Update README.java

--- README.java
+++ README.java
@@ -1,20 +1,4 @@
 /*
-# hello-world
-Obligatory tutorial repository
-
-
-A wise man once said that a journey of a thousand miles begins with a single step. While this simple branch/main is trivial, it is good to get the foundations of version control. I learned in school how much being able to 'cntrl-z' a project can matter. 
-
-Also because the tutorial said so: 
-
-Primary interest and language of choice is java, what I was tought in school and what I know. Also through school I have experience with css, html, javascript, mySql, and C++. Further I have some experience with Linux and drivers through my time with the robotics team, something that I would like to get back into later on down the road.
-
-
-2021_10_28 @ 19:43|| added breif description and personal info 
-*/
-
-
-/*
  * PACKAGE: com.example [I don't understand packages...*yet*, but I want to get used to adding this to the docs]
  * FILE: helloWorld
  * AUTHOR: Bradley M. WALKER ▐ 2021_10_18 @ ~01:15
